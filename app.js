16a674477341ccef789944869e82426b33573c1f
rekloyd <paumorillasrivera129@gmail.com>
2024-12-04 20:10:25 +0100

    index.html routed

--- app.js
+++ app.js
@@ -6,43 +6,63 @@ var url = require("url");
 var fs = require('fs');
 
 function iniciar() {
-	function onRequest(request, response) {
-		let sortida;
-        const baseURL = request.protocol + '://' + request.headers.host + '/';
-        const reqUrl = new URL(request.url, baseURL);
-        console.log("Petició per a  " + reqUrl.pathname + " rebuda.");
-        const pathname = reqUrl.pathname;
+  function onRequest(request, response) {
+    let sortida;
+    const baseURL = 'http://' + request.headers.host + '/';  // Arreglado el baseURL
+    const reqUrl = new URL(request.url, baseURL);  // Sin la "A"
+    console.log("Petició per a  " + reqUrl.pathname + " rebuda.");
+    const pathname = reqUrl.pathname;
 
-        if (pathname == '/') {
-            fs.readFile('./index.html', function(err, sortida) {
-                response.write(sortida);
-                response.end();
-            });
-        
-        } else if (pathname == './index.js') { // Cambiado './M01_ajax.js' a '/M01_ajax.js'
-            fs.readFile('./index.js', function(err, sortida) {
-                response.writeHead(200, {
-                    "Content-Type": "text/javascript; charset=utf-8"
-                });
-                response.write(sortida);
-                response.end();
-            });
-        
-        } else if (pathname == '/style.css') { // Cambiado './M01_styleAjax.css' a '/M01_styleAjax.css'
-            fs.readFile('./style.css', function(err, sortida) {
-                response.write(sortida);
-                response.end();
-            });
-        
+    // Ruta para la página principal (index.html)
+    if (pathname == '/') {
+      fs.readFile('./public/index.html', function (err, sortida) {
+        if (err) {
+          response.writeHead(500, { "Content-Type": "text/plain" });
+          response.write("Error al cargar index.html");
         } else {
-            sortida = "404 NOT FOUND";
-            response.write(sortida);
-            response.end();
+          response.writeHead(200, { "Content-Type": "text/html" });
+          response.write(sortida);
         }
-        
-	}
-	http.createServer(onRequest).listen(8888);
-	console.log("Servidor iniciat.");
+        response.end();
+      });
+
+    } else if (pathname == '/quizz.js') {  // Ruta para el archivo JavaScript
+      fs.readFile('./public/quizz.js', function (err, sortida) {
+        if (err) {
+          response.writeHead(500, { "Content-Type": "text/plain" });
+          response.write("Error al cargar index.js");
+        } else {
+          response.writeHead(200, { "Content-Type": "text/javascript; charset=utf-8" });
+          response.write(sortida);
+        }
+        response.end();
+      });
+
+    } else if (pathname == '/style.css') {  // Ruta para el archivo CSS
+      fs.readFile('./public/style.css', function (err, sortida) {
+        if (err) {
+          response.writeHead(500, { "Content-Type": "text/plain" });
+          response.write("Error al cargar style.css");
+        } else {
+          response.writeHead(200, { "Content-Type": "text/css; charset=utf-8" });
+          response.write(sortida);
+        }
+        response.end();
+      });
+
+    } else {  // Si no se encuentra la ruta solicitada
+      sortida = "404 NOT FOUND";
+      response.writeHead(404, { "Content-Type": "text/plain" });
+      response.write(sortida);
+      response.end();
+    }
+  }
+
+  /**Repetir este proceso para cada ruta */
+
+  http.createServer(onRequest).listen(8888, () => {
+    console.log("Servidor iniciat a http://localhost:8888");
+  });
 }
 
 exports.iniciar = iniciar;
