a230782697b65f95e3dc9a747b900c2a6edc1352
rekloyd <paumorillasrivera129@gmail.com>
2025-01-21 16:10:33 +0100

    refactored app code

--- app.js
+++ app.js
@@ -1,154 +1,74 @@
 /*
  * Servidor HTTP para el PJ9 de DAW2 en Jesuïtes el Clot
  */
-var http = require("http");
-var fs = require('fs');
-const mongoose = require('mongoose');
-const path = require('path');
-
-
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const mongoose = require("mongoose");
 
 function iniciar() {
   function onRequest(request, response) {
-    let sortida;
-    const baseURL = 'http://' + request.headers.host + '/';
+    const baseURL = `http://${request.headers.host}/`;
     const reqUrl = new URL(request.url, baseURL);
-    console.log("Petició per a  " + reqUrl.pathname + " rebuda.");
     const pathname = reqUrl.pathname;
 
-    // Manejo de las rutas
-    if (pathname == '/') {  // Ruta para la página principal (index.html)
-      fs.readFile('./public/index.html', function (err, sortida) {
+    console.log(`Petició per a ${pathname} rebuda.`);
+
+    // Define MIME types for different file extensions
+    const mimeTypes = {
+      ".html": "text/html",
+      ".css": "text/css",
+      ".js": "text/javascript",
+      ".jpg": "image/jpeg",
+      ".jpeg": "image/jpeg",
+      ".png": "image/png",
+      ".gif": "image/gif",
+      ".txt": "text/plain",
+    };
+
+    // Helper function to serve files
+    const serveFile = (filePath, contentType) => {
+      fs.readFile(filePath, (err, data) => {
         if (err) {
-          response.writeHead(500, { "Content-Type": "text/plain" });
-          response.write("Error al cargar index.html");
+          response.writeHead(404, { "Content-Type": "text/plain" });
+          response.end("404 Not Found");
         } else {
-          response.writeHead(200, { "Content-Type": "text/html" });
-          response.write(sortida);
+          response.writeHead(200, { "Content-Type": contentType });
+          response.end(data);
         }
-        response.end();
       });
-    
+    };
 
-    }
-    else if(pathname == '/inicio'){
-      fs.readFile('./public/inicio.html', function (err, sortida) {
-        if (err) {
-          response.writeHead(500, { "Content-Type": "text/plain" });
-          response.write("Error al cargar index.html");
-        } else {
-          response.writeHead(200, { "Content-Type": "text/html" });
-          response.write(sortida);
-        }
-        response.end();
-      });
-
-    } else if (pathname == '/quizz') {  // Ruta para el quiz (quizz.html)
-      fs.readFile('./public/quizz.html', function (err, sortida) {
-        if (err) {
-          response.writeHead(500, { "Content-Type": "text/plain" });
-          response.write("Error al cargar quizz.html");
-        } else {
-          response.writeHead(200, { "Content-Type": "text/html" });
-          response.write(sortida);
-        }
-        response.end();
-      });
-
-    } else if (pathname == '/quizz.js') {  // Ruta para el archivo JavaScript del quiz
-      fs.readFile('./public/quizz.js', function (err, sortida) {
-        if (err) {
-          response.writeHead(500, { "Content-Type": "text/plain" });
-          response.write("Error al cargar quizz.js");
-        } else {
-          response.writeHead(200, { "Content-Type": "text/javascript; charset=utf-8" });
-          response.write(sortida);
-        }
-        response.end();
-      });
-
-    } else if (pathname == '/styles.css') {  // Ruta para el archivo CSS
-      fs.readFile('./public/styles.css', function (err, sortida) {
-        if (err) {
-          response.writeHead(500, { "Content-Type": "text/plain" });
-          response.write("Error al cargar style.css");
-        } else {
-          response.writeHead(200, { "Content-Type": "text/css; charset=utf-8" });
-          response.write(sortida);
-        }
-        response.end();
-      });
-
-
-    }else if (pathname == '/contacto') {  // Ruta para el quiz (quizz.html)
-      fs.readFile('./public/contacto.html', function (err, sortida) {
-        if (err) {
-          response.writeHead(500, { "Content-Type": "text/plain" });
-          response.write("Error al cargar quizz.html");
-        } else {
-          response.writeHead(200, { "Content-Type": "text/html" });
-          response.write(sortida);
-        }
-        response.end();
-      });
-    }else if (pathname == '/graficas') {  // Ruta para el quiz (quizz.html)
-    fs.readFile('./public/graficas.html', function (err, sortida) {
-      if (err) {
-        response.writeHead(500, { "Content-Type": "text/plain" });
-        response.write("Error al cargar quizz.html");
-      } else {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write(sortida);
-      }
-      response.end();
-    },
-    fs.readFile('./public/graficas.js', function (err, sortida) {
-      if (err) {
-        response.writeHead(500, { "Content-Type": "text/plain" });
-        response.write("Error al cargar quizz.html");
-      } else {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write(sortida);
-      }
-      response.end();
-    }));
-  }else if (pathname == '/about') {
-    fs.readFile('./public/sobrenosotros.html', function (err, sortida) {
-      if (err) {
-        response.writeHead(500, { "Content-Type": "text/plain" });
-        response.write("Error al cargar quizz.html");
-      } else {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write(sortida);
-      }
-      response.end();
-    });
-  }else if (pathname.startsWith('/img/')) {  // Ruta para imágenes
-    fs.readFile('./public' + pathname, function (err, sortida) {
-      if (err) {
-        response.writeHead(404, { "Content-Type": "text/plain" });
-        response.write("Error al cargar la imagen");
-      } else {
-        // Detect the file extension for proper content type
-        const extname = path.extname(pathname);
-        let contentType = 'image/jpeg'; // Default to JPEG
-        if (extname === '.png') contentType = 'image/png';
-        if (extname === '.gif') contentType = 'image/gif';
-
-        response.writeHead(200, { "Content-Type": contentType });
-        response.write(sortida);
-      }
-      response.end();
-    });
-
-  }else {  // Si no se encuentra la ruta solicitada
-      sortida = "404 NOT FOUND";
+    // Route handling
+    if (pathname === "/") {
+      serveFile("./public/index.html", "text/html");
+    } else if (pathname === "/inicio") {
+      serveFile("./public/inicio.html", "text/html");
+    } else if (pathname === "/quizz") {
+      serveFile("./public/quizz.html", "text/html");
+    } else if (pathname === "/quizz.js") {
+      serveFile("./public/quizz.js", "text/javascript");
+    } else if (pathname === "/styles.css") {
+      serveFile("./public/styles.css", "text/css");
+    } else if (pathname === "/contacto") {
+      serveFile("./public/contacto.html", "text/html");
+    } else if (pathname === "/graficas") {
+      serveFile("./public/graficas.html", "text/html");
+    } else if (pathname === "/about") {
+      serveFile("./public/sobrenosotros.html", "text/html");
+    } else if (pathname.startsWith("/img/")) {
+      // Serve image files dynamically
+      const extname = path.extname(pathname);
+      const contentType = mimeTypes[extname] || "application/octet-stream";
+      serveFile(`./public${pathname}`, contentType);
+    } else {
+      // Handle 404 Not Found
       response.writeHead(404, { "Content-Type": "text/plain" });
-      response.write(sortida);
-      response.end();
+      response.end("404 NOT FOUND");
     }
   }
 
+  // Start the server
   http.createServer(onRequest).listen(8888, () => {
     console.log("Servidor iniciat a http://localhost:8888");
   });
